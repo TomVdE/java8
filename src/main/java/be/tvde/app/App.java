7da7219b1de5d3795e7300efdb58475762f468dc
Tom Van den Eynde <tom.vandeneynde@cegeka.be>
2016-11-07 20:59:39 +0100

    A dummy app

---
+++ src/main/java/be/tvde/app/App.java
@@ -0,0 +1,10 @@
+package be.tvde.app;
+
+/**
+ * Created by tomvde on 7/11/2016.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Just an app in the main package");
+    }
+}
